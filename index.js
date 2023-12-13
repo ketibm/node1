4e210b8d1e7660319fa5005959d1834049c1c20c
Your Name <you@example.com>
2023-12-13 23:39:20 +0100

    vezba

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 console.log("Keti");
+//HINT -> operation e string koj sodrzi +, -, /, *
+// function calculate(operation, a, b) {
+//switch
+//if
+// }
+
+// calculate("+", 4, 5);
+
+function calculate(operation, a, b) {
+  if (operation == "+") {
+    return a + b;
+  } else if (operation == "-") {
+    return a - b;
+  } else if (operation == "*") {
+    return a * b;
+  } else if (operation == "/") {
+    return a / b;
+  } else {
+    return "invalid input";
+  }
+  //   else if (operation == "-") return a - b;
+}
+console.log(calculate("/", 4, 5));
+
+function calculation(operation, a, b) {
+  switch (operation) {
+    case "+":
+      return a + b;
+      break;
+    case "-":
+      return a - b;
+      break;
+    case "*":
+      return a * b;
+      break;
+    case "/":
+      return a / b;
+      break;
+    default:
+      return "Invalid input";
+  }
+}
+console.log(calculation("-", 4, 5));
